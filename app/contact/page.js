0b367dc57edfe46fcea9ec2498e5f3e9a6e7fd43
faizi-uni <138822463+faizi-uni@users.noreply.github.com>
2025-12-09 13:36:48 +0100

    Improve layout structure and npm proxy config

---
+++ app/contact/page.js
@@ -0,0 +1,9 @@
+import Section from '../../components/Section';
+
+export default function ContactPage() {
+  return (
+    <main>
+      <Section id="kontakt" title="Kontakt" />
+    </main>
+  );
+}
